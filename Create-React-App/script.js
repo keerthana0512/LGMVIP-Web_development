0e8b21bd2874acada94883acd8b8c311489c3c90
Keerthana K R <97686515+keerthana0512@users.noreply.github.com>
2023-07-25 13:31:26 +0530

    Add files via upload

---
+++ Create-React-App/script.js
@@ -0,0 +1,48 @@
+const cardGrid = document.getElementById("cardGrid");
+const loader = document.getElementById("loader");
+
+function createCard(user) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const userImage = document.createElement("img");
+  userImage.src = user.avatar;
+  userImage.alt = `${user.first_name} ${user.last_name}`;
+
+  const cardTitle = document.createElement("h2");
+  cardTitle.textContent = `${user.first_name} ${user.last_name}`;
+
+  const cardEmail = document.createElement("p");
+  cardEmail.textContent = user.email;
+
+  card.appendChild(userImage);
+  card.appendChild(cardTitle);
+  card.appendChild(cardEmail);
+
+  return card;
+}
+async function getUsers() {
+    loader.style.display = "block";
+    cardGrid.innerHTML = "";
+    loader.innerHTML = '<div class="loader-spinner"></div><div class="loader-text">Loading...</div>'; // Add loader and text
+  
+    // Simulate a 10-second loading time
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+  
+    try {
+      const response = await fetch("https://reqres.in/api/users?page=1");
+      const data = await response.json();
+      const users = data.data;
+  
+      users.forEach((user) => {
+        const card = createCard(user);
+        cardGrid.appendChild(card);
+      });
+  
+      loader.style.display = "none";
+    } catch (error) {
+      console.error("Error fetching users:", error);
+      loader.style.display = "none";
+    }
+  }
+  
