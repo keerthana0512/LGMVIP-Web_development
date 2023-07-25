095b1c2bad0cfc5f09df362e943ffd58100b1b9f
Keerthana K R <97686515+keerthana0512@users.noreply.github.com>
2023-07-25 19:39:59 +0530

    Add files via upload

---
+++ To-Do-List/script.js
@@ -0,0 +1,34 @@
+const taskInput = document.getElementById("task-input");
+const taskList = document.getElementById("task-list");
+
+
+function addTask() {
+ 
+  if (taskInput.value === "") {
+         alert("Please Enter a Task");
+  }
+  else{
+  let li = document.createElement("li");
+  li.innerHTML = taskInput.value
+  taskList.appendChild(li);
+  let span = document.createElement("span");
+  span.innerHTML = '<i class="fas fa-trash"></i>';
+  li.appendChild(span);
+  li.lastElementChild.addEventListener("click", function(event) {
+  event.stopPropagation();
+  taskList.removeChild(li);
+  });
+  }
+taskInput.value=""
+}
+
+taskList.addEventListener("click", function(e){
+  if(e.target.matches("li")){
+    e.target.classList.toggle("completed");
+  }
+});
+
+function clearAll()
+{
+  taskList.innerHTML=""  
+}
